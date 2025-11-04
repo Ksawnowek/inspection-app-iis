2bd5375869a9dafe1a98aa21336b12b22ce31d5c
Kuboss949 <creazykuboss949@gmail.com>
2025-11-04 21:57:03 +0100

    quick-fix

--- backend/app/services/PDF_service.py
+++ backend/app/services/PDF_service.py
@@ -53,7 +53,7 @@ class PDFService:
             # Ale jeśli chcemy, by serwis wiedział, że nie ma danych - jest OK
             raise HTTPException(404, "Zadanie nie istnieje")
 
-        protokol_grupy = self._protokoly_service.get_protokol_details(pnagl_id)
+        protokol_grupy = self._protokoly_service.get_protokol_pozycje(pnagl_id)
 
         # Generowanie ścieżki
         out_path = self._pdf_dir / f"protokol_{pnagl}.pdf"
